33ba2b2738a8bb9ac1b6ad1ffb5bba099d6d4f45
DAVMONT08 <davide.montella@marconibari.edu.it>
2025-03-19 23:38:46 +0000

    CONSEGNSA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,66 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+              time_t now=time(0);
+              int N,i,j,t;
+              cout<<"quante cifre ci sono?"<<endl;
+              cin>>N;
+              vector<int>r[N];
+              for(i=0;i<N;i=i+1)
+              {
+              r[i]=random(11);
+              }
+              for(i=0;i<N;i=i+1)
+              {
+                j=0
+                while(j<=N-2)
+                {
+                    if(r[j]>r[j+1])
+                    {
+                        t=r[j];
+                        r[j]=r[j+1];
+                        r[j+1]=t;
+
+                    }
+                  j=j+1
+                }
+
+              }
+
+              j=0
+              while(j<N)
+              {
+                cout<<"r[j]"<<endl;
+                j=j+1
+
+            
+              }
+
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
